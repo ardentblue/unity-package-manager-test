5148ce16c41eaed82f285a3d3db5650f46ef14b0
Rob Bothof <rbothof@xs4all.nl>
2023-01-24 21:19:43 +0100

    added namespace for package code and samples

--- Runtime/HelloPluginBinding.cs
+++ Runtime/HelloPluginBinding.cs
@@ -2,23 +2,26 @@ using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class HelloPluginBinding : MonoBehaviour
-{
-    //Lets make our calls from the Plugin
-    [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
-    private static extern IntPtr PrintHello();
+namespace ABTestPackage {
 
-    [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
-    private static extern int PrintANumber();
-
-    [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
-    private static extern float AddTwoFloats(float f1, float f2);
-
-
-    void Start()
+    public class HelloPluginBinding : MonoBehaviour
     {
-        Debug.Log(Marshal.PtrToStringAnsi(PrintHello()));
-        Debug.Log(PrintANumber());
-        Debug.Log(AddTwoFloats(2.5F, 4F));
+        //Lets make our calls from the Plugin
+        [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr PrintHello();
+
+        [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int PrintANumber();
+
+        [DllImport("HelloPlugin", CallingConvention = CallingConvention.Cdecl)]
+        private static extern float AddTwoFloats(float f1, float f2);
+
+
+        void Start()
+        {
+            Debug.Log(Marshal.PtrToStringAnsi(PrintHello()));
+            Debug.Log(PrintANumber());
+            Debug.Log(AddTwoFloats(2.5F, 4F));
+        }
     }
 }
