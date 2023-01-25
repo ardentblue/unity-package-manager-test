8f25ad5df2f70eaf8377567bba8c7baf933b9178
Rob Bothof <rbothof@xs4all.nl>
2023-01-25 10:26:57 +0100

    changed version

--- Runtime/HelloPluginBinding.cs
+++ Runtime/HelloPluginBinding.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace ABTestPackage {
-
     public class HelloPluginBinding : MonoBehaviour
     {
         //Lets make our calls from the Plugin
